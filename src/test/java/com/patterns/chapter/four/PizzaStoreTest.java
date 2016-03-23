a0d0e84825e920f69a652473fde6af6a58e05dc7
Wender Freese <wender.jean@gmail.com>
2016-03-22 21:30:00 -0300

    Writing test in PizzaStoreTest

--- src/test/java/com/patterns/chapter/four/PizzaStoreTest.java
+++ src/test/java/com/patterns/chapter/four/PizzaStoreTest.java
@@ -7,4 +7,24 @@ import org.junit.Test;
 public class PizzaStoreTest {
 
 	private PizzaStore store = new PizzaStore(new SimplePizzaFactory());
+	
+	@Test
+	public void shouldMakeACheesePizza() {
+		assertThat(store.order("cheese").getClass(), equalTo(CheesePizza.class));
+	}
+	
+	@Test
+	public void shouldMakeAPepperoniPizza() {
+		assertThat(store.order("pepperoni").getClass(), equalTo(PepperoniPizza.class));
+	}
+	
+	@Test
+	public void shouldMakeAClamPizza() {
+		assertThat(store.order("clam").getClass(), equalTo(ClamPizza.class));
+	}
+	
+	@Test
+	public void shouldMakeAVeggiePizza() {
+		assertThat(store.order("veggie").getClass(), equalTo(VeggiePizza.class));
+	}
 }
