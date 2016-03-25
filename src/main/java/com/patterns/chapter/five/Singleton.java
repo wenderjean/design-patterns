48348c480b61535def123f0772091fcf4808e4f5
Wender Freese <wender.jean@gmail.com>
2016-03-24 22:31:29 -0300

    First version of classes available in Chapter 5

---
+++ src/main/java/com/patterns/chapter/five/Singleton.java
@@ -0,0 +1,17 @@
+package com.patterns.chapter.five;
+
+public class Singleton {
+	
+	private static Singleton instance;
+
+	private Singleton() {
+
+	}
+	
+	public static Singleton getInstance() {
+		if(instance == null) {
+			instance = new Singleton();
+		}
+		return instance;
+	}
+}
